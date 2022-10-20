c6dac4bf626b4c825f436248e38569eec3b7b428
Peter Yoachim <yoachim@uw.edu>
2022-10-20 13:48:31 -0700

    second round

--- draft/draft.sh
+++ draft/draft.sh
@@ -1,2 +1,2 @@
-python draft.py --survey_length 365 ; glance_dir --db draft_v2.99_1yrs.db
-python draft.py
+python draft_connected.py --survey_length 365 ; glance_dir --db draft_connected_v2.99_1yrs.db
+python draft_connected.py
