db3a9743dd4c5fd83b402144789926fa8e39ccb4
Peter Yoachim <yoachim@uw.edu>
2022-11-14 20:11:39 -0800

    fiddle footprint

--- franken_supress/franken_s.sh
+++ franken_supress/franken_s.sh
@@ -5,3 +5,10 @@ python franken_supress.py --repeat_weight 10
 python franken_supress.py --repeat_weight 20
 python franken_supress.py --repeat_weight 50
 python franken_supress.py --repeat_weight 100
+python franken_supress_fp.py --repeat_weight 0
+python franken_supress_fp.py --repeat_weight 1
+python franken_supress_fp.py --repeat_weight 2
+python franken_supress_fp.py --repeat_weight 10
+python franken_supress_fp.py --repeat_weight 20
+python franken_supress_fp.py --repeat_weight 50
+python franken_supress_fp.py --repeat_weight 100
