85752d96c44a6412fd08792e5dcabd3e32811ce2
Peter Yoachim <yoachim@uw.edu>
2022-11-21 19:32:14 -0800

    typo

---
+++ draft3/draft3.sh
@@ -0,0 +1,18 @@
+python draft3.py --ddf_per 8
+python draft3.py --ddf_per 7.5
+python draft3.py --ddf_per 7
+python draft3.py --ddf_per 6.5
+python draft3.py --ddf_per 6.
+python draft3.py --ddf_per 5.5
+python draft3.py --ddf_per 5.
+python draft3.py --ddf_per 4.5
+python draft3.py --ddf_per 4
+python draft3.py --uzy --ddf_per 8
+python draft3.py --uzy --ddf_per 7.5
+python draft3.py --uzy --ddf_per 7
+python draft3.py --uzy --ddf_per 6.5
+python draft3.py --uzy --ddf_per 6.
+python draft3.py --uzy --ddf_per 5.5
+python draft3.py --uzy --ddf_per 5.
+python draft3.py --uzy --ddf_per 4.5
+python draft3.py --uzy --ddf_per 4
